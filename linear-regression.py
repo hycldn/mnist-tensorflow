39142f729f656f56d8a7c10e121736ee5bd1157f
Rishabh Thukral <rishi.feb1@gmail.com>
2017-06-16 03:40:07 +0530

    Adding code for experimentation session with tensor flow.

---
+++ linear-regression.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+# @Author: Rishabh Thukral
+# @Date:   2017-06-16 01:03:44
+# @Last Modified by:   Rishabh Thukral
+# @Last Modified time: 2017-06-16 01:05:58
+# @Description: Linear Regression using tensor flow core
+
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
+import numpy as np
+import tensorflow as tf
+
+# Model parameters
+W = tf.Variable([.3], tf.float32)
+b = tf.Variable([-.3], tf.float32)
+
+# Model input and output
+x = tf.placeholder(tf.float32)
+linear_model = W * x + b
+y = tf.placeholder(tf.float32)
+
+# loss
+loss = tf.reduce_sum(tf.square(linear_model - y)) # sum of the squares
+
+# optimizer
+optimizer = tf.train.GradientDescentOptimizer(0.01)
+train = optimizer.minimize(loss)
+
+# training data
+x_train = [1,2,3,4]
+y_train = [0,-1,-2,-3]
+
+# training loop
+init = tf.global_variables_initializer()
+sess = tf.Session()
+
+sess.run(init) # reset values to wrong
+
+for i in range(1000):
+  sess.run(train, {x:x_train, y:y_train})
+
+# evaluate training accuracy
+curr_W, curr_b, curr_loss  = sess.run([W, b, loss], {x:x_train, y:y_train})
+print("W: %s b: %s loss: %s"%(curr_W, curr_b, curr_loss))
